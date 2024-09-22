a30df650d503d64292d56fc2374fffd04c9d16b2
Andrés <cabeltran@correo.url.edu.gt>
2024-09-21 21:10:04 -0600

    Primeros intentos de grabacion con audio a partir de la funcion QtAudio

--- master.py
+++ master.py
@@ -2,6 +2,8 @@ import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
 from DESIGN_FILES.main_window import Ui_MainWindow
 from DESIGN_FILES.record_design import Ui_RecordOptions
+from FUNCTIONS import recorder
+from FUNCTIONS.recorder import AudioRecorder
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -28,6 +30,20 @@ class RecordOptions(QMainWindow):
         super(RecordOptions, self).__init__()
         self.ui = Ui_RecordOptions()
         self.ui.setupUi(self)
+
+        # Instanciando la clase Recorder para manejar la grabación
+
+        self.recorder = recorder(self)
+
+        # Conectando el botón de grabación con la función que comienza la misma
+        self.ui.IniciarGrabacion.clicked.connect(self.iniciar_grabacion)
+
+    def iniciar_grabacion(self):
+
+        duration_seconds = self.ui.TiempoGrabacion.value()
+        format_selected = self.ui.SeleccionFormatoGrabacion.currentText()
+
+        self.recorder.start_recording(duration_seconds, format_selected)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
